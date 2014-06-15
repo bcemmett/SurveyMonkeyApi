936addca5e03ce52923917c0665d43629e41361d
bcemmett <ben.emmett@red-gate.com>
2014-06-15 12:55:31 +0200

    Initial attempt at getting survey overview details

---
+++ SurveyMonkeyApi/Containers.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyMonkeyApi
+{
+
+    public class Survey
+    {
+        public long survey_id;
+        public DateTime date_created; //can json.net serialize these?
+        public DateTime date_modified;
+        public string title;
+        public int language_id;
+        public int question_count;
+        public int num_responses;
+        public string analysis_url;
+        public string preview_url;
+
+        //fields from [get_survey_list], [get_survey_details], [get_collector_list], and possibly [get_response_counts]
+    }
+
+    public class Response
+    {
+        //fields from [get_responses]
+    }
+
+    public class Respondent
+    {
+        //fields from [get_respondent_list]
+    }
+
+    public class SettingsGetSurveyList
+    {
+        public int page;
+        public int page_size;
+        public DateTime start_date = DateTime.MaxValue; //TODO change these to datetimes?
+        public DateTime end_date = DateTime.MinValue;
+        public string title;
+        public string recipient_email;
+        public bool order_asc;
+        
+        internal RequestSettings Serialized
+        {
+            get
+            {
+                var parameters = new RequestSettings();
+
+                if (start_date != DateTime.MaxValue)
+                {
+                    parameters.Add("start_date", start_date.ToString("yyyy-MM-dd HH':'mm':'ss"));
+                }
+                if (end_date != DateTime.MinValue)
+                {
+                    parameters.Add("end_date", end_date.ToString("yyyy-MM-dd HH':'mm':'ss"));
+                }
+                if (!String.IsNullOrEmpty(title))
+                {
+                    parameters.Add("title", title);
+                }
+                if (!String.IsNullOrEmpty(recipient_email))
+                {
+                    parameters.Add("recipient_email", recipient_email);
+                }
+                if (order_asc)
+                {
+                    parameters.Add("order_asc", "True");
+                }
+                return parameters;
+            }
+        }
+
+        public SettingsGetSurveyList()
+        {
+        }
+    }
+
+    public class RequestSettings : Dictionary<string, object>
+    {
+    }
+}
---
+++ SurveyMonkeyApi/SMApi.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Security.Policy;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SurveyMonkeyApi
+{
+    public class SMApi
+    {
+        #region Members
+        private string m_ApiKey;
+        private string m_OAuthSecret;
+        private string m_BaseUrl = "https://api.surveymonkey.net/v2";
+        private long m_LastRequestTime = 0;
+        private int m_RequestDelay = 600;
+        #endregion
+
+
+        ///No limit on page size
+        public List<Survey> GetSurveyListPage(SettingsGetSurveyList settings, int page)
+        {
+            if (page < 1)
+            {
+                throw new ArgumentException("Page must be greater than 0.");
+            }
+            return GetSurveyListPage(settings, page, 0, false);            
+        }
+
+        //Limit the page size returned
+        public List<Survey> GetSurveyListPage(SettingsGetSurveyList settings, int page, int pageSize)
+        {
+            if (pageSize < 1 || pageSize > 1000)
+            {
+                throw new ArgumentException("Page size must be between 1 and 1000.");
+            }
+            return GetSurveyListPage(settings, page, pageSize, true);
+        }
+
+        private List<Survey> GetSurveyListPage(SettingsGetSurveyList settings, int page, int pageSize, bool limitPageSize )
+        {
+            RequestSettings parameters = settings.Serialized;
+            parameters.Add("page", page);
+            if (limitPageSize)
+            {
+                parameters.Add("page_size", pageSize);
+            }
+            return GetSurveyList(parameters);
+        }
+
+        private List<Survey> GetSurveyList(RequestSettings parameters)
+        {
+            const string endPoint = "/surveys/get_survey_list";
+            parameters.Add("fields", new List<string> { "title", "analysis_url", "preview_url", "date_created", "date_modified", "language_id", "question_count", "num_responses" });
+            var o = MakeApiRequest(endPoint, parameters);
+
+            var surveys = JsonConvert.DeserializeObject<List<Survey>>(o.SelectToken("surveys").ToString());
+
+            return surveys;
+        }
+
+        #region API communication guts
+
+        //TODO: Allow custom delay and custom base urls here
+        public SMApi(string apiKey, string oAuthSecret)
+        {
+            if (apiKey == null || oAuthSecret == null)
+            {
+                throw new ArgumentNullException();
+            }
+            m_ApiKey = apiKey;
+            m_OAuthSecret = oAuthSecret;
+        }
+
+        private JToken MakeApiRequest(string endPoint, Dictionary<string, object> data)
+        {
+            RateLimit();
+
+            string url = m_BaseUrl + endPoint;
+            var serializedParameters = JsonConvert.SerializeObject(data);
+            string result;
+
+            using (var webClient = new WebClient())
+            {
+                webClient.Encoding = System.Text.Encoding.UTF8;
+                webClient.Headers.Add("Content-Type", "application/json");
+                webClient.Headers.Add("Authorization", "Bearer " + m_OAuthSecret);
+                webClient.QueryString.Add("api_key", m_ApiKey);
+                result = webClient.UploadString(url, "POST", serializedParameters);
+            }
+
+            var o = JObject.Parse(result);
+            CheckSurveyMonkeyResponseIsValid(o);
+            return o.SelectToken(("data"));
+        }
+
+        private void RateLimit()
+        {
+            //TODO: don't be as wasteful if <500ms but not 0ms of stuff happened since last request
+            if (m_LastRequestTime != 0 && (DateTime.Now.Millisecond - m_LastRequestTime < m_RequestDelay))
+            {
+                Thread.Sleep(m_RequestDelay);
+            }
+            m_LastRequestTime = DateTime.Now.Millisecond;
+        }
+
+        private void CheckSurveyMonkeyResponseIsValid(JObject o)
+        {
+            if ((int)o["status"] != 0)
+            {
+                string msg = "Problem querying the Survey Monkey API, error code "
+                             + (int) o["status"]
+                             + " (https://developer.surveymonkey.com/mashery/requests_responses)";
+                throw new WebException(msg);
+            }
+        }
+        #endregion
+
+    }
+}
