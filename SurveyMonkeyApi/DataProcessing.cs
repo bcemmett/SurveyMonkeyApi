e3a2ed35abdb5377ac739fa7336e4529d57bb3cb
bcemmett <ben.emmett@red-gate.com>
2014-06-29 11:26:58 +0100

    Fill in complete survey details

---
+++ SurveyMonkeyApi/DataProcessing.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyMonkeyApi
+{
+    public partial class SMApi
+    {
+        public void FillMissingSurveyDetails(Survey survey)
+        {
+            Survey surveyDetails = GetSurveyDetails(survey.survey_id);
+            survey.date_created = surveyDetails.date_created;
+            survey.date_modified = surveyDetails.date_modified;
+            survey.language_id = surveyDetails.language_id;
+            survey.num_responses = surveyDetails.num_responses;
+            survey.question_count = surveyDetails.question_count;
+            survey.nickname = surveyDetails.nickname;
+            survey.title_text = surveyDetails.title_text;
+            survey.title_enabled = surveyDetails.title_enabled;
+            survey.pages = surveyDetails.pages;
+        }
+
+        public void FillMissingSurveyDetails(List<Survey> surveys)
+        {
+            foreach (var survey in surveys)
+            {
+                FillMissingSurveyDetails(survey);
+            }
+        }
+    }
+}
